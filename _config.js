9394837d7927fd37281c9f16f3b8516863196130
c-kiplimo <kiplimocollins855@gmail.com>
2024-06-29 13:18:31 +0300

    fix connection to db

--- _config.js
+++ _config.js
@@ -1,10 +1,9 @@
-var config = {}
+var config = {};
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://ckiplimo54:zZtixYKJpVhtjXjW@devops.itcjcnm.mongodb.net/?retryWrites=true&w=majority&appName=devops    ',
-    development: 'mongodb+srv://ckiplimo54:zZtixYKJpVhtjXjW@devops.itcjcnm.mongodb.net/?retryWrites=true&w=majority&appName=devops',
-    test: 'mongodb+srv://ckiplimo54:zZtixYKJpVhtjXjW@devops.itcjcnm.mongodb.net/?retryWrites=true&w=majority&appName=devops',
-}
-module.exports = config;
+    production: 'mongodb+srv://ckiplimo54:PhQSPyLFkjpIEPFf@devops.itcjcnm.mongodb.net/?retryWrites=true&w=majority&appName=devops',
+    development: 'mongodb+srv://ckiplimo54:PhQSPyLFkjpIEPFf@devops.itcjcnm.mongodb.net/?retryWrites=true&w=majority&appName=devops',
+    test: 'mongodb+srv://ckiplimo54:PhQSPyLFkjpIEPFf@devops.itcjcnm.mongodb.net/?retryWrites=true&w=majority&appName=devops'
+};
 
+module.exports = config;
