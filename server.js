9d15e371ed60d4c3cee5ffc3568afde6d0685a88
c-kiplimo <kiplimocollins855@gmail.com>
2024-06-30 15:59:09 +0300

    update db configuration

--- server.js
+++ server.js
@@ -1,46 +1,40 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const dotenv = require('dotenv');
+const config = require('./_config'); 
 
-// Define routes
-let index = require('./routes/index');
-let image = require('./routes/image');
+dotenv.config(); 
 
-// connecting the database
-let mongodb_url = 'mongodb+srv://ckiplimo54:Ngetich1234@gallery.bn3rhdm.mongodb.net/darkroom?retryWrites=true&w=majority';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
+const env = process.env.NODE_ENV || 'development';
+const mongodb_url = config.mongoURI[env];
+
+mongoose.connect(mongodb_url, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Database connected successfully');
+    }
 });
 
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log('Database connected successfully');
+});
 
-// Initializing the app
 const app = express();
 
-
-// View Engine
 app.set('view engine', 'ejs');
-
-// Set up the public folder;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-// body parser middleware
-app.use(express.json())
-
+const index = require('./routes/index');
+const image = require('./routes/image');
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
 });
