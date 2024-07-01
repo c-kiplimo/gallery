6a718ac556b0b2f34cfb1f764dbec7694cda51a3
c-kiplimo <kiplimocollins855@gmail.com>
2024-07-01 23:22:32 +0300

    update project tittle

--- _config.js
+++ _config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const config = {
     mongoURI: {
         production: 'mongodb+srv://ckiplimo54:Ngetich1234@gallery.bn3rhdm.mongodb.net/darkroom?retryWrites=true&w=majority',
@@ -7,14 +6,4 @@ const config = {
     }
 };
 
-=======
-var config = {}
-
-// Update to have your correct username and password
-config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
->>>>>>> test
 module.exports = config;
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-<<<<<<< HEAD
 const dotenv = require('dotenv');
 const config = require('./_config'); 
-=======
-const config = require('./_config');
->>>>>>> test
 
 dotenv.config(); 
 
-<<<<<<< HEAD
 const env = process.env.NODE_ENV || 'development';
 const mongodb_url = config.mongoURI[env];
 
@@ -28,29 +23,6 @@ db.once('open', () => {
 });
 
 const app = express();
-=======
-// Initializing the app
-const app = express();
-
-// connecting the database
-
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
-    if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
-    }
-});
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
-
->>>>>>> test
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -63,15 +35,6 @@ app.use('/', index);
 app.use('/image', image);
 
 const PORT = process.env.PORT || 5000;
-<<<<<<< HEAD
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
 });
-=======
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
-});
-
-
-module.exports = app;
->>>>>>> test
